88a07fd0b6dc423c2c84cb3528b82b09cbe182e7
Earth-And-Moon <108616786+Earth-And-Moon@users.noreply.github.com>
2026-07-16 23:49:33 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ run_every = 10.0	# sec	1
 log_every = 30.0	# sec	3
 
 # 5h, 6h is limit
-run_for = 18000		#28800		# end script after x sec	10	295
+run_for = 21000		#18000		#28800		# end script after x sec	10	295
 
 
 def do_it():
